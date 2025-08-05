916c3a92b9b53943e6f988bd5cfc4d773bf55091
Shyamal Patel <patelshyamal016@gmail.com>
2025-08-05 17:36:35 -0400

    Refactored Code Smell 5: ClassName: DashboardController.java, Code Smell: Number of Parameters

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -73,7 +73,7 @@ public class DashboardController {
         double totalCalories = 0, totalProtein = 0, totalCarbs = 0, totalFats = 0, totalFiber = 0;
         double totalSodium = 0, totalSugars = 0, totalSaturatedFats = 0, totalIron = 0, totalCalcium = 0;
         double totalVitaminA = 0, totalVitaminB = 0, totalVitaminC = 0, totalVitaminD = 0;
-        
+
         for (Meal meal : meals) {
             totalCalories += meal.getTotalCalories();
             totalProtein += meal.getTotalProtein();
@@ -90,10 +90,23 @@ public class DashboardController {
             totalVitaminC += meal.getTotalVitaminC();
             totalVitaminD += meal.getTotalVitaminD();
         }
-        
-        return new NutritionSummary(totalCalories, totalProtein, totalCarbs, totalFats, totalFiber,
-                                  totalSodium, totalSugars, totalSaturatedFats, totalIron, totalCalcium,
-                                  totalVitaminA, totalVitaminB, totalVitaminC, totalVitaminD);
+
+        return new NutritionSummary.Builder()
+            .totalCalories(totalCalories)
+            .totalProtein(totalProtein)
+            .totalCarbs(totalCarbs)
+            .totalFats(totalFats)
+            .totalFiber(totalFiber)
+            .totalSodium(totalSodium)
+            .totalSugars(totalSugars)
+            .totalSaturatedFats(totalSaturatedFats)
+            .totalIron(totalIron)
+            .totalCalcium(totalCalcium)
+            .totalVitaminA(totalVitaminA)
+            .totalVitaminB(totalVitaminB)
+            .totalVitaminC(totalVitaminC)
+            .totalVitaminD(totalVitaminD)
+            .build();
     }
     
     /**
@@ -120,23 +133,23 @@ public class DashboardController {
      */
     private NutritionSummary calculateAverageNutrition(NutritionSummary total, int days) {
         if (days <= 0) days = 1;
-        
-        return new NutritionSummary(
-            total.totalCalories / days,
-            total.totalProtein / days,
-            total.totalCarbs / days,
-            total.totalFats / days,
-            total.totalFiber / days,
-            total.totalSodium / days,
-            total.totalSugars / days,
-            total.totalSaturatedFats / days,
-            total.totalIron / days,
-            total.totalCalcium / days,
-            total.totalVitaminA / days,
-            total.totalVitaminB / days,
-            total.totalVitaminC / days,
-            total.totalVitaminD / days
-        );
+
+        return new NutritionSummary.Builder()
+            .totalCalories(total.totalCalories / days)
+            .totalProtein(total.totalProtein / days)
+            .totalCarbs(total.totalCarbs / days)
+            .totalFats(total.totalFats / days)
+            .totalFiber(total.totalFiber / days)
+            .totalSodium(total.totalSodium / days)
+            .totalSugars(total.totalSugars / days)
+            .totalSaturatedFats(total.totalSaturatedFats / days)
+            .totalIron(total.totalIron / days)
+            .totalCalcium(total.totalCalcium / days)
+            .totalVitaminA(total.totalVitaminA / days)
+            .totalVitaminB(total.totalVitaminB / days)
+            .totalVitaminC(total.totalVitaminC / days)
+            .totalVitaminD(total.totalVitaminD / days)
+            .build();
     }
     
     /**
@@ -202,26 +215,47 @@ public class DashboardController {
         public final double totalCalories, totalProtein, totalCarbs, totalFats, totalFiber;
         public final double totalSodium, totalSugars, totalSaturatedFats, totalIron, totalCalcium;
         public final double totalVitaminA, totalVitaminB, totalVitaminC, totalVitaminD;
-        
-        public NutritionSummary(double totalCalories, double totalProtein, double totalCarbs, 
-                              double totalFats, double totalFiber, double totalSodium, 
-                              double totalSugars, double totalSaturatedFats, double totalIron, 
-                              double totalCalcium, double totalVitaminA, double totalVitaminB, 
-                              double totalVitaminC, double totalVitaminD) {
-            this.totalCalories = totalCalories;
-            this.totalProtein = totalProtein;
-            this.totalCarbs = totalCarbs;
-            this.totalFats = totalFats;
-            this.totalFiber = totalFiber;
-            this.totalSodium = totalSodium;
-            this.totalSugars = totalSugars;
-            this.totalSaturatedFats = totalSaturatedFats;
-            this.totalIron = totalIron;
-            this.totalCalcium = totalCalcium;
-            this.totalVitaminA = totalVitaminA;
-            this.totalVitaminB = totalVitaminB;
-            this.totalVitaminC = totalVitaminC;
-            this.totalVitaminD = totalVitaminD;
+
+        private NutritionSummary(Builder builder) {
+            this.totalCalories = builder.totalCalories;
+            this.totalProtein = builder.totalProtein;
+            this.totalCarbs = builder.totalCarbs;
+            this.totalFats = builder.totalFats;
+            this.totalFiber = builder.totalFiber;
+            this.totalSodium = builder.totalSodium;
+            this.totalSugars = builder.totalSugars;
+            this.totalSaturatedFats = builder.totalSaturatedFats;
+            this.totalIron = builder.totalIron;
+            this.totalCalcium = builder.totalCalcium;
+            this.totalVitaminA = builder.totalVitaminA;
+            this.totalVitaminB = builder.totalVitaminB;
+            this.totalVitaminC = builder.totalVitaminC;
+            this.totalVitaminD = builder.totalVitaminD;
+        }
+
+        public static class Builder {
+            private double totalCalories, totalProtein, totalCarbs, totalFats, totalFiber;
+            private double totalSodium, totalSugars, totalSaturatedFats, totalIron, totalCalcium;
+            private double totalVitaminA, totalVitaminB, totalVitaminC, totalVitaminD;
+
+            public Builder totalCalories(double val) { totalCalories = val; return this; }
+            public Builder totalProtein(double val) { totalProtein = val; return this; }
+            public Builder totalCarbs(double val) { totalCarbs = val; return this; }
+            public Builder totalFats(double val) { totalFats = val; return this; }
+            public Builder totalFiber(double val) { totalFiber = val; return this; }
+            public Builder totalSodium(double val) { totalSodium = val; return this; }
+            public Builder totalSugars(double val) { totalSugars = val; return this; }
+            public Builder totalSaturatedFats(double val) { totalSaturatedFats = val; return this; }
+            public Builder totalIron(double val) { totalIron = val; return this; }
+            public Builder totalCalcium(double val) { totalCalcium = val; return this; }
+            public Builder totalVitaminA(double val) { totalVitaminA = val; return this; }
+            public Builder totalVitaminB(double val) { totalVitaminB = val; return this; }
+            public Builder totalVitaminC(double val) { totalVitaminC = val; return this; }
+            public Builder totalVitaminD(double val) { totalVitaminD = val; return this; }
+
+            public NutritionSummary build() {
+                return new NutritionSummary(this);
+            }
         }
     }
     
